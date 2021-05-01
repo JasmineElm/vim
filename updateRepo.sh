1842157b6961ea5bcd87f595e2ccdc4ffae22b51
James Lemin <kj6fi170m6e@riseup.net>
2021-05-01 11:31:53 +0100

    sync: 2021-05-01 11:31

--- updateRepo.sh
+++ updateRepo.sh
@@ -1,24 +1,32 @@
-#!/bin/bash
-
-
-
-timestamp() {
-  date +"%d-%m-%Y @ %T"
-}
+#!/usr/bin/env bash
 
 update() {
   # Explicitly copy local files rather than rely on symlinks
-  rsync $HOME/.vimrc .
-  rsync $HOME/.vim/templates/* .vim/templates/
-  rsync $HOME/.vim/local_functions.vim .vim/local_functions.vim
+  rsync "$HOME"/.vimrc .
+  rsync "$HOME"/.vim/templates/* .vim/templates/
+  rsync "$HOME"/.vim/local_functions.vim .vim/local_functions.vim
 }
 
-commit() {
-  if [[ $(git status --porcelain) ]]; then
-      git add .
-      git commit -m "Update: $(timestamp)"
-  fi
+datestamp() {
+  date +"%Y-%m-%d %H:%M"
+} 
+
+pushit() {
+  git pull
+  git add .
+  git commit -q -m "sync: $(datestamp)"
+  git push
 }
 
-update
-commit
+main() {
+  # update, and push any changes
+  update
+  out_of_sync=$(git status --porcelain | wc -l)
+  [ "$out_of_sync" -eq 0 ] || pushit
+}
+
+main
+
+
+
+
